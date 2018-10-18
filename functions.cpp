baafcd8b1d494134c7c58409fd8d79204b155f2c
lucqiiiii <qi_guo@umail.ucsb.edu>
2018-10-18 15:49:39 -0700

    done

---
+++ functions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <time.h>
+#include "functions.h"
+
+using namespace std;
+
+void sayHello ()
+{
+  cout << "Hello!" << endl;
+}
+
+void printDate()
+{
+  time_t rawtime;
+  time ( &rawtime );
+  cout << "The current local time is "
+       << ctime (&rawtime) << endl;
+}
+
